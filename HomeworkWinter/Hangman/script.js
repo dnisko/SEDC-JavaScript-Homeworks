ec32ce5247baec9b210f01d813b6ff82c6d6d5c9
dnisko <dnisko@hotmail.com>
2024-01-08 21:32:07 +0100

    Finished Hangman

--- HomeworkWinter/Hangman/script.js
+++ HomeworkWinter/Hangman/script.js
@@ -11,84 +11,153 @@ divBottom.hidden = true;
 let hangmanArray = [];
 let buttonClicked = [];
 
-// let restart = document.getElementById("btnRestart");
+let clues = document.getElementById("clue");
 let chosenWord = document.getElementById("word");
 let chosenCategory = document.getElementById("category");
 let chooseCategory = document.getElementById("chooseCategory");
 let livesText = document.getElementById("lives");
 let lives = 6;
+
+for(let i = 0; i < lives; i++)
+{
+    // console.log(document.getElementById(`img${i}`));
+    document.getElementById(`img${i}`).hidden = true;
+}
+
 let usedLetters = document.getElementById("usedLetters");
 let word = "";
 let dashedArray = [];
 
-addToHangman("The Matrix", "Movie");
-addToHangman("Strawberry", "Food");
-addToHangman("JavaScript", "IT");
-addToHangman("Dire Straits", "Music");
-addToHangman("Testing with dash - tested", "Test");
-addToHangman("Never gonna give you up, never gonna let you down", "Music");
-addToHangman("A Series of Unfortunate Events", "Movie");
-addToHangman("Bolognese Shpagetti", "Food");
-addToHangman("Never gonna give you up, never gonna let you down", "Food");
-addToHangman("Diego Armando Maradona", "Sport");
+let clueArray = [];
+let depletedClues = false;
 
-// console.log(hangmanArray);
-
-//https://www.geeksforgeeks.org/how-to-get-all-unique-values-remove-duplicates-in-a-javascript-array/
 let categoryArray = [];
 let count = 0;
 let start = false;
 
-for(let i = 0; i < hangmanArray.length; i++)
+addToHangman("The Matrix", "Movie", ["Green", "Falling letters", "Computer rulling"]);
+addToHangman("Strawberry", "Food", ["Clue1", "Clue2", "Clue3"]);
+addToHangman("JavaScript", "IT", ["Clue1", "Clue2", "Clue3"]);
+addToHangman("Dire Straits", "Music", ["Clue1", "Clue2", "Clue3"]);
+addToHangman("Testing with dash - tested", "Test", ["Clue1", "Clue2", "Clue3"]);
+addToHangman("Never gonna give you up, never gonna let you down", "Music", ["Clue1", "Clue2", "Clue3"]);
+addToHangman("A Series of Unfortunate Events", "Movie", ["Clue1", "Clue2", "Clue3"]);
+addToHangman("Bolognese Shpagetti", "Food", ["Clue1", "Clue2", "Clue3"]);
+addToHangman("Never gonna give you up, never gonna let you down", "Food", ["Clue1", "Clue2", "Clue3"]);
+addToHangman("Diego Armando Maradona", "Sport", ["Clue1", "Clue2", "Clue3"]);
+
+
+//https://www.geeksforgeeks.org/how-to-get-all-unique-values-remove-duplicates-in-a-javascript-array/
+
+
+if(categoryArray.length === 0)
 {
-    for(let j = 0; j < categoryArray.length ;j++)
+    getUniqueCategory(hangmanArray, categoryArray, count, start);
+}
+function getUniqueCategory(hangmanArray, categoryArray, count, start)
+{
+    for(let i = 0; i < hangmanArray.length; i++)
     {
-        if(hangmanArray[i].category == categoryArray[j])
+        for(let j = 0; j < categoryArray.length ;j++)
         {
-            start = true
+            if(hangmanArray[i].category == categoryArray[j])
+            {
+                start = true
+            }
         }
-    }
-    count++;
+        count++;
 
-    if(count === 1 && start === false)
+        if(count === 1 && start === false)
+        {
+            categoryArray.push(hangmanArray[i].category);
+        }
+        start = false;
+        count = 0;
+    }
+
+    // console.log(categoryArray);
+
+    for(let i = 0; i < categoryArray.length; i++)
     {
-        categoryArray.push(hangmanArray[i].category);
+        // console.log(categoryArray[i]);
+        let add = document.createElement("option");
+        add.value = i;
+        add.innerHTML = categoryArray[i];
+        // console.log(hangmanArray[i].category);
+        chooseCategory.appendChild(add);
     }
-    start = false;
-    count = 0;
 }
 
-// console.log(categoryArray);
 
-for(let i = 0; i < categoryArray.length; i++)
-{
-    // console.log(categoryArray[i]);
-    let add = document.createElement("option");
-    add.value = i;
-    add.innerHTML = categoryArray[i];
-    // console.log(hangmanArray[i].category);
-    chooseCategory.appendChild(add);
-}
-
-function Hangman(word, category)
+function Hangman(word, category, clue = [])
 {
     this.word = word;
     this.category = category;
+    this.clue = clue;
 }
 
-function addToHangman(word, category)
+function addToHangman(word, category, clue)
 {
     // console.log(hangmanArray);
-    hangmanArray.push(new Hangman(word, category));
+    hangmanArray.push(new Hangman(word, category, clue));
     // console.log(hangmanArray);
 }
-
 
 function getRandomInt(min, max)
 {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getClues(array, word)
+{
+    console.log("clicked the clear button");
+    // console.log(hangmanArray);
+    // console.log(word);
+    // console.log(hangmanArray[1].clue);
+    
+    if(clueArray.length === 0 && depletedClues === false)
+    {
+        for(let i = 0; i < array.length; i++)
+        {
+            if(hangmanArray[i].word === word)
+            {
+                // console.log(array[i].clue[2]);
+                // console.log(array[i].clue.length);
+                // clueArray.push(hangmanArray[i].clue);
+                for(let j = 0; j < array[i].clue.length; j++)
+                {
+                    clueArray.push(array[i].clue[j]);
+                }
+            }
+            // console.log(clueArray);
+        }
+
+        
+    }
+    // console.log(clueArray);
+    if(clueArray.length > 0 && depletedClues === false)
+    {
+        clues.innerText += `${clueArray[0]}\n`;
+        // console.log(clueArray);
+        clueArray.shift();
+        // console.log(clueArray);
+
+        if(clueArray.length === 0)
+        {
+            clues.innerText += `\nNo more clues, you are on your own now.`;
+            depletedClues = true;
+        }
+    }
+           
+        console.log(clueArray);
+    
+    
+    for(let i = 0; i < clueArray.length; i++)
+    {
+
+    }
 }
 
 chooseCategory.onchange = function ()
@@ -123,7 +192,7 @@ chooseCategory.onchange = function ()
     word = hangmanFromCategory[random];
 
     console.log(`${word}, ${category}`);
-
+    console.log(word);
     chosenCategory.innerText = `The chosen category is: ${category}`;
 
     for(let i = 0; i < word.length; i++)
@@ -144,12 +213,14 @@ chooseCategory.onchange = function ()
             chosenWord.innerHTML += `_&nbsp;`;
         }
     }
-    console.log(word.length);
-    console.log(dashedArray);
+    // console.log(word.length);
+    // console.log(dashedArray);
 }
+
 //https://www.fwait.com/how-to-detect-which-button-is-clicked-in-javascript/
 document.addEventListener('click', function(e)
 {
+    console.log(hangmanArray);
     let toLower = word.toLowerCase();
     let element = e.target;
     // console.log("tuka");
@@ -159,19 +230,25 @@ document.addEventListener('click', function(e)
         buttonClicked = [element.id, element.innerText];
         console.log(buttonClicked);
     }
+
     if(buttonClicked[0] === "btnRestart")
     {
-        console.log("tuka....");
         reset();
     }
+
+    if(buttonClicked[0] === "btnClue")
+    {
+        getClues(hangmanArray, word);
+    }
+
     let btn = document.getElementById(`${buttonClicked[1]}Btn`);
     
     if(btn !== null)
     {
-      if(btn.disabled === false)
+        if(btn.disabled === false)
         {
             let doesLetterEixsist = false;
-            // console.log(buttonClicked[1]);
+                // console.log(buttonClicked[1]);
             for(let i = 0; i < word.length; i++)
             {
                 if(toLower[i] == buttonClicked[1])
@@ -184,10 +261,10 @@ document.addEventListener('click', function(e)
                     doesLetterEixsist = true;
 
                     //Wierd text style and adding ,...
-                    chosenWord.innerHTML = dashedArray;
+                    chosenWord.innerHTML = dashedArray.join(" ");
                 }
             }
-            
+                
             // console.log(btn.getAttribute("disabled"));
             // console.log(btn.disabled);
             btn.disabled = true;
@@ -195,33 +272,94 @@ document.addEventListener('click', function(e)
             if(!doesLetterEixsist)
             {
                 lives += -1;
+                if(lives === 0)
+                {
+                    if (confirm(`You have lost, the chosen word was ${word}`) == true)
+                    {
+                        reset();
+                    }
+                    else
+                    {
+                        reset();
+                    }
+                }
             }
-            console.log(lives);
-            console.log(doesLetterEixsist);
-
+            // console.log(lives);
+            // console.log(doesLetterEixsist);
             usedLetters.innerHTML += `${element.innerText}, `;
         }
 
-        livesText.innerText = lives;  
-    }
-    
+        livesText.innerText = lives;
+        if(lives < 6)
+        {
+            // console.log(lives);
+            hangmanPicture(lives);
+        }
+        // }
+        // console.log(dashedArray.join(""));
+        // console.log("this");
+        // console.log(word);
+        if(dashedArray.join("") == word)
+        {
+            if (confirm(`Congratulations!!! The chosen word was ${word}`) == true)
+            {
+                reset();
+            }
+            else
+            {
+                reset();
+            }
+        }
+    } 
+   
 });
+
+function hangmanPicture(lives)
+{
+    tempLives = lives + 1;
+    console.log(tempLives);
+    console.log(document.getElementById(`img${lives}`));
+    document.getElementById(`img${lives}`).hidden = false;
+    
+
+}
 
 function reset()
 {
-    dashedArray = [];
+    // dashedArray = null;
 
     divMiddle.hidden = true;
     divLeft.hidden = true;
     divMiddle1.hidden = true;
     divBottom.hidden = true;
 
+    lives = 6;
     livesText.innerText = lives;
 
     chosenWord.innerHTML = "";
     buttonClicked = [];
+
+    clues.innerHTML = "";
+    depletedClues = false;
+
+    for (i = 97; i <= 122; i++)
+    {
+        document.getElementById(`${String.fromCharCode(i)}Btn`).disabled = false;
+    }
+
+    // hangmanArray = [];
+    buttonClicked = [];
+
+    for(let i = 0; i < lives; i++)
+    {
+        // console.log(document.getElementById(`img${i}`));
+        document.getElementById(`img${i}`).hidden = true;
+    }
+
+    dashedArray = [];
+    usedLetters.innerHTML = "";
+    // clueArray = [];
+
+    // categoryArray = [];
+    // location.reload();
 }
-    // divMiddle.hidden = false;
-    // divLeft.hidden = false;
-    // divMiddle1.hidden = false;
-    // divBottom.hidden = false;
